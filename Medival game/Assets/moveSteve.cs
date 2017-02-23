21b20d6c54655a4fe3471288ee94f86d189927c4
STUDENT\mr8347y <mr8347y@greenwich.ac.uk>
2017-02-23 12:09:30 +0000

    File upload

---
+++ Medival game/Assets/moveSteve.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveSteve : MonoBehaviour {
+	float timeCheck = 1f;
+	float timePassed;
+	public Transform target;
+
+	NavMeshAgent na;
+
+	// Use this for initialization
+	void Start () {
+		na = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timePassed > timeCheck) {
+			timePassed = 0f;
+			na.SetDestination (target.position);
+		}
+		timePassed += Time.deltaTime;
+	}
+		
+}
